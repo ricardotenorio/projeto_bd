b340e2affbb29743830f9d0e8745f420fb89326b
cyopse <lucas.ta@outlook.com>
2022-04-13 00:54:57 -0300

    Cliente, clienteJuridico e clienteFisico

--- src/main/java/model/entity/Cliente.java
+++ src/main/java/model/entity/Cliente.java
@@ -2,25 +2,22 @@ package model.entity;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table(name = "CLIENTE")
-public class Cliente implements Serializable {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipo_cliente")
+public abstract class Cliente implements Serializable {
 	
 	private static final long serialVersionUID = 3387642521978418140L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
 	@Column(name = "ID_CLIENTE")
 	private Long id;
 	
-	@Column(name = "NOME", nullable = false, length = 45)
+	@Column(name = "NOME", nullable = false)
 	private String nome;
 	
 	public Cliente() {}
