47b13fe52a89789b01ad57e28469feaeb0816b1e
ricardotenorio <ricardo.heber@gmail.com>
2022-04-17 21:39:30 -0300

    adicionadas funções para calcular o valor total do pedido em updates e deletes

--- etc/sql/trigger_function.sql
+++ etc/sql/trigger_function.sql
@@ -1,3 +1,4 @@
+-- INSERT
 CREATE OR REPLACE FUNCTION calcular_valor_total()
 	RETURNS TRIGGER
 	LANGUAGE PLPGSQL
@@ -15,3 +16,41 @@ CREATE TRIGGER trigger_atualizar_valor_pedido
 	ON pedido_produto
 	FOR EACH ROW
 	EXECUTE PROCEDURE calcular_valor_total();
+
+-- UPDATE
+CREATE OR REPLACE FUNCTION calcular_valor_total_update()
+	RETURNS TRIGGER
+	LANGUAGE PLPGSQL
+AS $$
+BEGIN
+	UPDATE pedido
+	SET valor_total = COALESCE(valor_total, 0) - (OLD.valor_unidade * OLD.qtd) + (NEW.valor_unidade * NEW.qtd)
+	WHERE id_pedido = NEW.id_pedido;
+	RETURN NULL;
+END;
+$$;
+
+CREATE TRIGGER trigger_atualizar_valor_pedido_update
+	AFTER UPDATE
+	ON pedido_produto
+	FOR EACH ROW
+	EXECUTE PROCEDURE calcular_valor_total_update();
+
+-- DELETE
+CREATE OR REPLACE FUNCTION calcular_valor_total_delete()
+	RETURNS TRIGGER
+	LANGUAGE PLPGSQL
+AS $$
+BEGIN
+	UPDATE pedido
+	SET valor_total = COALESCE(valor_total, 0) - (OLD.valor_unidade * OLD.qtd)
+	WHERE id_pedido = OLD.id_pedido;
+	RETURN NULL;
+END;
+$$;
+
+CREATE TRIGGER trigger_atualizar_valor_pedido_delete
+	AFTER DELETE
+	ON pedido_produto
+	FOR EACH ROW
+	EXECUTE PROCEDURE calcular_valor_total_delete();
